c703f7052ede2a8cc493854ed57525cb98365a0d
Raymond W. Robinson <raymondrobinson123@gmail.com>
2018-03-25 20:31:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {};
 
-function updatePlaylist(playlist, artistName, songTitle){
+function updatePlaylist(playlist, key, value){
   playlist.artistName = "Phil Ochs";
   playlist.songTitle = "Here's to the State of Mississippi";
   return playlist;
