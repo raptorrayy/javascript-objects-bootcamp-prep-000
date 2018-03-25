62c281afc007cb048a45f28482e005f705d12dc8
Raymond W. Robinson <raymondrobinson123@gmail.com>
2018-03-25 20:26:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,6 @@
 var playlist = {artistName, songTitle};
 
-function updatePlaylist(obj, artistName, songTitle)
+function updatePlaylist(obj, artistName, songTitle){
+  this.obj = playlist;
+  artistName = ""
+}
